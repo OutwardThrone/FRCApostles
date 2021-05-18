436d56ccc2de9677eb337b6f147b91573957661a
Vibhav Peri <vibhavperi@gmail.com>
2021-05-18 13:36:35 -0700

    Initial UI

---
+++ src/Database/Main.java
@@ -0,0 +1,18 @@
+package Database;
+
+import javax.swing.*;
+
+public class Main {
+    public static int HEIGHT = 600;
+    public static int WIDTH = 800;
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        AppPanel app = new AppPanel();
+        frame.add(app);
+
+    }
+}
